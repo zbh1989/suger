c5ef3602aaa28eb4756b5350912d006b1c156872
ez6041007@163.com <zyd@871x>
2023-08-03 21:01:18 +0800

    增加广告

--- lib/pages/swiperPage.dart
+++ lib/pages/swiperPage.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:caihong_app/pages/showSwiperPage.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../base/view/base_state.dart';
 import '../presenter/swiperPagePresenter.dart';
+import '../utils/toast.dart';
 
 class SwiperPage extends StatefulWidget {
 
@@ -79,9 +81,21 @@ class SwiperPageState extends BaseState<SwiperPage,SwiperPagePresenter> {
           autoplay: true,
           duration:200,
           onTap: (index) {
-            Navigator.push(context, MaterialPageRoute(builder: (context){
-              return ShowSwiperPage(widget.imgList[index]["img"]);
-            }));
+            var title = widget.imgList[index]["title"];
+            if(title == '1'){
+              String url = 'https://fds.bjbsst.com';
+              canLaunch(url).then((canOpen){
+                if(canOpen){
+                  launch(url);
+                }else{
+                  Toast.show('无法打开网页地址: $url');
+                }
+              });
+            }else{
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                return ShowSwiperPage(widget.imgList[index]["img"]);
+              }));
+            }
           },
         ));
   }
